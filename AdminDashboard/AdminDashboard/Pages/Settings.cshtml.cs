ffd0aa3b05ff004243e42805c560b95646f933c8
aevangel <aleksandar.evangelatov@progress.com>
2021-08-02 18:10:47 +0300

    add initial Settings page

--- AdminDashboard/AdminDashboard/Pages/Settings.cshtml.cs
+++ AdminDashboard/AdminDashboard/Pages/Settings.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,8 +10,38 @@ namespace AdminDashboard.Pages
 {
     public class SettingsModel : PageModel
     {
+        [BindProperty]
+        public UserDetailsModel UserDetails { get; set; }
+        [BindProperty]
+        public UserInformationModel UserInformation { get; set; }
         public void OnGet()
         {
+            if (UserDetails == null)
+            {
+                UserDetails = new UserDetailsModel();
+            }
+            if (UserInformation == null)
+            {
+                UserInformation = new UserInformationModel();
+            }
+        }
+        public class UserDetailsModel
+        {
+            [Required]
+            public string Username { get; set; }
+            [Required]
+            public string Nickname { get; set; }
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+            public string Phone { get; set; }
+        }
+        public class UserInformationModel
+        {
+            public DateTime BirthDate { get; set; }
+            public string Country { get; set; }
+            public string Website { get; set; }
+            public string WorkPhone { get; set; }
         }
     }
 }

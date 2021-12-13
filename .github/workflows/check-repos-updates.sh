45e1f1f52e320c3c8bd675b2a6e2fb69f864f3dc
Mihail Parvanov <mihail.parvanov@progress.com>
2021-12-13 15:49:46 +0200

    chore: Test workflow

--- .github/workflows/check-repos-updates.sh
+++ .github/workflows/check-repos-updates.sh
@@ -85,18 +85,18 @@ function create_pr {
     reviewers="mparvanov"
     BRANCH_NAME="update-dependencies"
     PRs=$(GITHUB_TOKEN=$TOKEN gh pr list | grep "$BRANCH_NAME" || true)
-    if [ ! -z $PRs ]; then
-        echo "Unmerged pr $BRANCH_NAME"
-    else
-        git fetch origin
-        git checkout -b $BRANCH_NAME
-        git config user.email "kendo-bot@progress.com"
-        git config user.name "kendo-bot"
-        git add package.json && git commit -m "chore: update dependencies"
-        git push -u origin $BRANCH_NAME
-        GITHUB_TOKEN=$TOKEN \
-        gh pr create --base master --head $BRANCH_NAME --reviewer $reviewers \
-        --title "Update dependencies $DATE" --body 'Please review and update dependencies'
+    # if [ ! -z $PRs ]; then
+    #     echo "Unmerged pr $BRANCH_NAME"
+    # else
+    #     git fetch origin
+    #     git checkout -b $BRANCH_NAME
+    #     git config user.email "kendo-bot@progress.com"
+    #     git config user.name "kendo-bot"
+    #     git add package.json && git commit -m "chore: update dependencies"
+    #     git push -u origin $BRANCH_NAME
+    #     GITHUB_TOKEN=$TOKEN \
+    #     gh pr create --base master --head $BRANCH_NAME --reviewer $reviewers \
+    #     --title "Update dependencies $DATE" --body 'Please review and update dependencies'
     fi
 }
 

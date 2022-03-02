1edcf269eb0994b5846e1a2c57e454c6ddd00e66
aevangel <aleksandar.evangelatov@progress.com>
2022-03-02 15:55:09 +0200

    update settings page with new design

--- AdminDashboard/AdminDashboard/Pages/Settings.cshtml.cs
+++ AdminDashboard/AdminDashboard/Pages/Settings.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 
 namespace AdminDashboard.Pages
@@ -28,6 +29,16 @@ namespace AdminDashboard.Pages
         public UserDetailsModel UserDetails { get; set; }
         [BindProperty]
         public UserInformationModel UserInformation { get; set; }
+        [BindProperty]
+        public UserSecurityModel UserSecurityDetails { get; set; }
+        [BindProperty]
+        public PaymentDetailsModel PaymentDetails { get; set; }
+        [BindProperty]
+        public NotificationAlertsModel NotificationAlerts { get; set; }
+        [BindProperty]
+        public NotificationEmailsModel NotificationEmails { get; set; }
+        [BindProperty]
+        public List<SelectListItem> CountryOptions { get; set; }
         public async Task OnGetAsync()
         {
             ClaimsPrincipal currentUser = User;
@@ -52,6 +63,62 @@ namespace AdminDashboard.Pages
                     Country = "1",
                 };
             }
+            if (UserSecurityDetails == null)
+            {
+                UserSecurityDetails = new UserSecurityModel()
+                {
+                    CurrentPassword = "",
+                    NewPassword = "",
+                    Enable2FA = loggedUser.TwoFactorEnabled,
+                    RequestChange = true
+                };
+            }
+            if (CountryOptions == null)
+            {
+                CountryOptions = new List<SelectListItem>() {
+                                  new SelectListItem() {
+                                      Text = "USA",
+                                      Value = "1"
+                                  },
+                                  new SelectListItem() {
+                                      Text = "United Kingdom",
+                                      Value = "2"
+                                  },
+                                  new SelectListItem() {
+                                      Text = "Bulgaria",
+                                      Value = "3"
+                                  }
+                              };
+            }
+            if (PaymentDetails == null)
+            {
+                PaymentDetails = new PaymentDetailsModel() 
+                {
+                    CardHolder = loggedUser.FullName,
+                    CardHolderCountry = "1",
+                    ZipCode = "12345"
+                          
+                };
+            }
+            if (NotificationAlerts == null)
+            {
+                NotificationAlerts = new NotificationAlertsModel()
+                {
+                    Communication = true,
+                    Security = false,
+                    Meetups = true
+                };
+            }
+            if (NotificationEmails == null)
+            {
+                NotificationEmails = new NotificationEmailsModel()
+                {
+                    Photos = false,
+                    Mentions = false,
+                    ReplyToGroup = true
+                };
+            }
+
         }
         public class UserDetailsModel
         {
@@ -70,6 +137,36 @@ namespace AdminDashboard.Pages
             public string Country { get; set; }
             public string Website { get; set; }
             public string WorkPhone { get; set; }
+        }
+
+        public class UserSecurityModel
+        {
+            public string CurrentPassword { get; set; }
+            public string NewPassword { get; set; }
+            public bool Enable2FA { get; set; }
+            public bool RequestChange { get; set; }
+        }
+
+        public class PaymentDetailsModel
+        {
+            public string CardHolder { get; set; }
+            public string CardNumber { get; set; }
+            public DateTime? ExpirationDate { get; set; }
+            public string CVCNumber { get; set; }
+            public string CardHolderCountry { get; set; }
+            public string ZipCode { get; set; }
+        }
+        public class NotificationAlertsModel
+        {
+            public bool Communication { get; set; }
+            public bool Security { get; set; }
+            public bool Meetups { get; set; }
+        }
+        public class NotificationEmailsModel
+        {
+            public bool Photos { get; set; }
+            public bool Mentions { get; set; }
+            public bool ReplyToGroup { get; set; }
         }
     }
 }
